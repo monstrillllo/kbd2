40516a6c697142a5b890eb57f1e93a6c2112eb85
monstrillllp <evgeniy27112000@gmail.com>
2020-11-07 20:31:25 +0300

    commit1234

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class Application(tk.Frame):
 
     def create_widgets(self):
         self.createbtn(self, "Add ingredient", self.btn1Window)
-        self.createbtn(self, "Edit ingredient", self.btn1Window)
-        self.createbtn(self, "Delete ingredient", self.btn1Window)
+        self.createbtn(self, "Edit ingredient", self.btn2Window)
+        self.createbtn(self, "Delete ingredient", self.btn3Window)
         self.createbtn(self, "Add recipe", self.btn1Window)
         self.createbtn(self, "Edit recipe", self.btn1Window)
         self.createbtn(self, "Delete recipe", self.btn1Window)
@@ -52,35 +52,74 @@ class Application(tk.Frame):
     def disconnect(self, conn):
         conn.close()
 
-    # command for btn1
     def btn1Window (self):
         self.btn1Window = tk.Toplevel(root)
         self.btn1Window.title("Add ingredient")
+
         self.ingr_group_id = tk.IntVar()
         self.calories = tk.StringVar()
         self.provider_id = tk.StringVar()
         self.ingredient_name = tk.StringVar()
-        self.label1 = tk.Label(self.btn1Window, text="Ingredient group ID")
-        self.label1.grid(row=0, column=0, ipadx=10, ipady=6, padx=10, pady=10)
-        self.label2 = tk.Label(self.btn1Window, text="Calories")
-        self.label2.grid(row=0, column=1, ipadx=10, ipady=6, padx=10, pady=10)
-        self.label3 = tk.Label(self.btn1Window, text="Supplier ID")
-        self.label3.grid(row=0, column=2, ipadx=10, ipady=6, padx=10, pady=10)
-        self.label4 = tk.Label(self.btn1Window, text="Ingredient name")
-        self.label4.grid(row=0, column=3, ipadx=10, ipady=6, padx=10, pady=10)
-        self.group_entry = tk.Entry(master=self.btn1Window, textvariable=self.ingr_group_id)
-        self.group_entry.grid(row=1, column=0, ipadx=10, ipady=6, padx=10, pady=10)
-        self.calories_entry = tk.Entry(master=self.btn1Window, textvariable=self.calories)
-        self.calories_entry.grid(row=1, column=1, ipadx=10, ipady=6, padx=10, pady=10)
-        self.provider_id_entry = tk.Entry(master=self.btn1Window, textvariable=self.provider_id)
-        self.provider_id_entry.grid(row=1, column=2, ipadx=10, ipady=6, padx=10, pady=10)
-        self.ingredient_name_entry = tk.Entry(master=self.btn1Window, textvariable=self.ingredient_name)
-        self.ingredient_name_entry.grid(row=1, column=3, ipadx=10, ipady=6, padx=10, pady=10)
+
+        self.createLabel(self.btn1Window, "Ingredient group ID", 0, 0)
+        self.createLabel(self.btn1Window, "Calories", 0, 1)
+        self.createLabel(self.btn1Window, "Supplier ID", 0, 2)
+        self.createLabel(self.btn1Window, "Ingredient name", 0, 3)
+
+        self.createEntery(self.btn1Window, self.ingr_group_id, 1, 0)
+        self.createEntery(self.btn1Window, self.calories, 1, 1)
+        self.createEntery(self.btn1Window, self.provider_id, 1, 2)
+        self.createEntery(self.btn1Window, self.ingredient_name, 1, 3)
+
         self.createbtn(self.btn1Window, "Add", self.addtokb, 2, 1)
         self.createbtn(self.btn1Window, "QUIT", self.btn1Window.destroy, 2, 2)
 
-    # def createEntery(self, master, textvariable):
+    def btn2Window(self):
+        self.btn2Window = tk.Toplevel(root)
+        self.btn2Window.title("Edit ingredient")
 
+        self.igredient_edit = tk.StringVar
+        self.new_ingr_group_id = tk.IntVar()
+        self.new_calories = tk.StringVar()
+        self.new_provider_id = tk.StringVar()
+        self.new_ingredient_name = tk.StringVar()
+
+        self.createLabel(self.btn2Window, "Ingredient to edit", 0, 0)
+        self.createLabel(self.btn2Window, "New ingredient group ID", 0, 1)
+        self.createLabel(self.btn2Window, "New calories", 0, 2)
+        self.createLabel(self.btn2Window, "New supplier ID", 0, 3)
+        self.createLabel(self.btn2Window, "New ingredient name", 0, 4)
+
+        self.createEntery(self.btn2Window, self.igredient_edit, 1, 0)
+        self.createEntery(self.btn2Window, self.new_ingr_group_id, 1, 1)
+        self.createEntery(self.btn2Window, self.new_calories, 1, 2)
+        self.createEntery(self.btn2Window, self.new_provider_id, 1, 3)
+        self.createEntery(self.btn2Window, self.new_ingredient_name, 1, 4)
+
+        self.createbtn(self.btn2Window, "Edit", self.addtokb, 2, 0)
+        self.createbtn(self.btn2Window, "QUIT", self.btn2Window.destroy, 2, 4)
+
+    def btn3Window(self):
+        self.btn3Window = tk.Toplevel(root)
+        self.btn3Window.title("Delete ingredient")
+
+        self.igredient_delete = tk.StringVar
+
+        self.createLabel(self.btn3Window, "Ingredient to delete", 0, 1)
+
+        self.createEntery(self.btn3Window, self.igredient_delete, 1, 1)
+
+
+        self.createbtn(self.btn3Window, "Delete", self.addtokb, 1, 0)
+        self.createbtn(self.btn3Window, "QUIT", self.btn3Window.destroy, 1, 2)
+
+    def createLabel(self, master, text, row, column):
+        self.newLabel = tk.Label(master=master, text=text)
+        self.newLabel.grid(row=row, column=column, ipadx=10, ipady=6, padx=10, pady=10)
+
+    def createEntery(self, master, textvariable, row , column):
+        self.new_entry = tk.Entry(master=master, textvariable=textvariable)
+        self.new_entry.grid(row=row, column=column, ipadx=10, ipady=6, padx=10, pady=10)
 
     def addtokb(self):
         try:
@@ -99,5 +138,6 @@ class Application(tk.Frame):
 
 
 root = tk.Tk()
+root.title("Food")
 app = Application(master=root)
 app.mainloop()
